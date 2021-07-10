e2b65236a0bd8220fccd4eb7c345b66b53dc242b
arunkumar-reddy <arunkumarreddy1997@gmail.com>
2021-07-10 19:21:53 +0530

    feat: adding discover widget

---
+++ Android/app/src/main/java/com/arun/android/moviedb/screens/discover/DiscoverWidget.kt
@@ -0,0 +1,32 @@
+package com.arun.android.moviedb.screens.discover
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.arun.android.moviedb.utils.ColorUtils
+import com.arun.moviedb.sdk.models.discover.DiscoverResult
+
+@Composable
+fun DiscoverWidget(title: String, data: List<DiscoverResult>) {
+    Column(modifier = Modifier.padding(16.dp)) {
+        Text( text = title, fontSize = 20.sp, fontWeight = FontWeight.Bold, color = ColorUtils.getColorFromHex("#FFC107"))
+        Row(modifier = Modifier.padding(top = 16.dp).horizontalScroll(rememberScrollState())) {
+            data.forEach { discoverResult ->
+                Column(modifier = Modifier.width(100.dp)) {
+                    Text(text = discoverResult.title, fontSize = 15.sp, color = ColorUtils.getColorFromHex("#000000"))
+                }
+            }
+        }
+    }
+}
--- Android/app/src/main/java/com/arun/android/moviedb/screens/home/HomeScreen.kt
+++ Android/app/src/main/java/com/arun/android/moviedb/screens/home/HomeScreen.kt
@@ -1,7 +1,17 @@
 package com.arun.android.moviedb.screens.home
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.arun.android.moviedb.screens.discover.DiscoverWidget
+import com.arun.android.moviedb.utils.ColorUtils
 import com.arun.moviedb.sdk.dispatcher.ActionDispatcher
 import com.arun.moviedb.sdk.dispatcher.actions.Action
 import com.arun.moviedb.sdk.dispatcher.actions.ActionTypes
@@ -13,10 +23,38 @@ import kotlinx.coroutines.launch
 
 @Composable
 fun HomeScreen(viewModel: HomeScreenViewModel, dispatcher: ActionDispatcher) {
-    if (!viewModel.hasDataLoaded) {
-        Text("Home Screen Data has not loaded")
-        dispatcher.dispatch(Action(ActionTypes.LOAD_HOME_SCREEN))
-        return
+    Column(modifier = Modifier.fillMaxSize()) {
+        if (!viewModel.hasDataLoaded) {
+            Row(modifier = Modifier.align(Alignment.CenterHorizontally), verticalAlignment = Alignment.CenterVertically) {
+                Loader()
+            }
+            dispatcher.dispatch(Action(ActionTypes.LOAD_HOME_SCREEN))
+        } else {
+            Column(modifier = Modifier.verticalScroll(rememberScrollState())) {
+                viewModel.popularMovies?.let {
+                    DiscoverWidget(title = "Popular Movies", data = it)
+                }
+                viewModel.latestMovies?.let {
+                    DiscoverWidget(title = "Latest Movies", data = it)
+                }
+                viewModel.popularTvShows?.let {
+                    DiscoverWidget(title = "Popular Tv Shows", data = it)
+                }
+                viewModel.latestTvShows?.let {
+                    DiscoverWidget(title = "Latest Tv Shows", data = it)
+                }
+                viewModel.regionalMovies?.let {
+                    DiscoverWidget(title = "Regional Movies", data = it)
+                }
+            }
+        }
     }
-    Text("Home Screen data has loaded with movies: " + viewModel.popularMovies?.size)
+}
+
+@Composable
+fun Loader() {
+    CircularProgressIndicator(
+        modifier = Modifier.size(40.dp),
+        color = ColorUtils.getColorFromHex("#FFC107")
+    )
 }
