3b6e034548b5ff0000b0736aa75d25d5e5931401
arunkumar-reddy <arunkumarreddy1997@gmail.com>
2021-07-17 23:32:33 +0530

    feat: adding sdk as a dependency for the stencil app

--- Stencil/movie-db/src/components/app-container/AppContainer.tsx
+++ Stencil/movie-db/src/components/app-container/AppContainer.tsx
@@ -1,10 +1,20 @@
-import { Component, h } from '@stencil/core';
+import { Component, h, State } from '@stencil/core';
+import {} from 'kotlin/kotlin'
+const sdk = (window as any).moviesdk.com.arun.moviedb.sdk as any;
 
 @Component({
   tag: 'app-container',
   shadow: true,
 })
 export class AppContainer {
+  application: any;
+  @State() appState: any;
+
+  componentWillLoad() {
+    this.application = new sdk.Application()
+    console.log(this.application.appState);
+  }
+
   render() {
     return (
       <div>
