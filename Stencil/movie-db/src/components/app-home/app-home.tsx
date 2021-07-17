b22b0304b6afb3c9698ca8c7610027cdc05ec897
arunkumar-reddy <arunkumarreddy1997@gmail.com>
2021-07-17 11:51:50 +0530

    chore: delete unnecessary test files

--- Stencil/movie-db/src/components/app-home/app-home.tsx
+++ Stencil/movie-db/src/components/app-home/app-home.tsx
@@ -13,10 +13,7 @@ export class AppHome {
           Welcome to the Stencil App Starter. You can use this starter to build entire apps all with web components using Stencil! Check out our docs on{' '}
           <a href="https://stenciljs.com">stenciljs.com</a> to get started.
         </p>
-
-        <stencil-route-link url="/profile/stencil">
-          <button>Profile page</button>
-        </stencil-route-link>
+        <button>Profile page</button>
       </div>
     );
   }
